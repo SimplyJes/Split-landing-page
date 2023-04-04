5b51c594d94f75548ab494a7cb81a08f584b5c69
Adedokun Oluwadurotimi Adeyemi <113611366+SimplyJes@users.noreply.github.com>
2023-04-04 20:11:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 
 iphone.addEventListener('click', () => {
-  window.open(alert('Thank you for your patronage!👏'), _blank)
+  window.open('https://www.samsung.com/us/smartphones/galaxy-s23-ultra/buy/galaxy-s23-ultra-256gb-t-mobile-sm-s918uzgaxau/?adobe_mc_sdid=SDID%3D680C7D293B96A091-7FF31374C279BC25%7CMCORGID%3D48855C6655783A647F000101%40AdobeOrg%7CTS%3D1680618151&adobe_mc_ref=https%3A%2F%2Fwww.google.com%2F', _blank)
 })
 samsung.addEventListener('click', () => {
-  window.open(alert('We love you!❤'), _blank)
+  window.open('https://www.apple.com/ng/iphone/', _blank)
 })
 
 left.addEventListener('mouseover', () => {
