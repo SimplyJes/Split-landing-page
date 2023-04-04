0fccf1aeaebe288d655a7ee36721951a9e7d09aa
Adedokun Oluwadurotimi Adeyemi <113611366+SimplyJes@users.noreply.github.com>
2023-04-04 20:24:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const right = document.querySelector('.right')
 
 iphone.addEventListener('click', () => {
   window.open('https://www.apple.com/ng/iphone/')
+  event.preventDefault();
 })
 samsung.addEventListener('click', () => {
   window.open('https://www.samsung.com/us/smartphones/galaxy-s23-ultra/buy/galaxy-s23-ultra-256gb-t-mobile-sm-s918uzgaxau/?adobe_mc_sdid=SDID%3D680C7D293B96A091-7FF31374C279BC25%7CMCORGID%3D48855C6655783A647F000101%40AdobeOrg%7CTS%3D1680618151&adobe_mc_ref=https%3A%2F%2Fwww.google.com%2F')
+  event.preventDefault();
 })
 
 left.addEventListener('mouseover', () => {
